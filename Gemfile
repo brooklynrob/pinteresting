ac8d31196f28cfb105f79219829b6e25636a0240
Robert Underwood <runderwood5@gmail.com>
2015-05-17 19:21:04 -0400

    Wiring in Devise

--- Gemfile
+++ Gemfile
@@ -23,7 +23,10 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
+
+
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.4.1'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Suggested by Devise -- OneMonthRails says "Requird by Heroku"
+  config.action_mailer.default_url_options = { host: 'dabestwebsite.com', port: 3000 }
+
 end
